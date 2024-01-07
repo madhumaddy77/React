9cd18ae674cb643bd4b6840dac3c43aca9893db2
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2024-01-08 00:51:45 +0530

    react setup

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+  <h1>Hello aur react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+
+  return (
+    <h1>hello aur react with vite</h1>
+     
+  )
+}
+
+export default App
