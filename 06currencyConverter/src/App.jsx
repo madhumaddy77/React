e8c1f32294f80973690783d2519118e85c2ccf60
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2024-01-24 23:44:35 +0530

    initial stage

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+      <h1 className="text-3xl  bg-orange-500 ">
+       Currency App with me
+      </h1>
+    </>
+  )
+}
+
+export default App
