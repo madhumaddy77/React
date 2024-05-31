37dc0515656e4764e3178abac9be8c2e9b6b20d7
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2024-06-01 00:07:05 +0530

    react with appwrite

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf={
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwritePROJECT_ID: String(import.meta.env.VITE_PROJECT_ID),
+    appwritePDATABASE_ID: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCOLLECTION_ID: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBUCKET_ID: String(import.meta.env.VITE_BUCKET_ID),
+    
+}
+
+export default conf
