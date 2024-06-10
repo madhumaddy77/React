b597a099d7ee02e7e343c97fbe3c988e54235e7c
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2024-06-10 20:57:07 +0530

    appwrite backebd code

--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf={
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwritePROJECT_ID: String(import.meta.env.VITE_PROJECT_ID),
-    appwritePDATABASE_ID: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteDATABASE_ID: String(import.meta.env.VITE_DATABASE_ID),
     appwriteCOLLECTION_ID: String(import.meta.env.VITE_COLLECTION_ID),
     appwriteBUCKET_ID: String(import.meta.env.VITE_BUCKET_ID),
     
