e7b9b3ceadab5b17962d83d4bb4a39bb3338d037
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2024-06-24 12:33:01 +0530

    redux implementation finished

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,48 @@
-
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth.js'
+import {login,logout} from './store/authSlice.js'
+import {Header,Footer} from './components'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const[loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
 
-  return (
-  <>
-  <h1> Blog app with Appwrite</h1>
-  </>
-  )
-}
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch (logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+     }, [])
+
+
+     return !loading ?  (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header/>
+      <main>
+      TODO : <outlet/>
+      </main>
+      <Footer/>
+      </div>
+      </div>
+     ) : null
+
+
+    }
+  
+
+
+
+
+  
+
 
 export default App
